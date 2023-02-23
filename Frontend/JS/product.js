e810bff88575e47a624cc866175418e051946ce8
Soumya Adhya <soumyaadhya007@gmail.com>
2023-02-23 20:19:41 +0530

    day-3 soumya

--- Frontend/JS/product.js
+++ Frontend/JS/product.js
@@ -1,14 +1,11 @@
 let baseurl="https://powerful-erin-jewelry.cyclic.app"
 
 let preloader= document.getElementById("loading");
-
-// let section_product=document.getElementById("product-view-section")
-
-// section_product.addEventListener("load",loadFun)
-
 function loadingFun(){
   preloader.style.display="none"
 }
+
+
 // let size=""
 function sortFilter() {
     let select = document.getElementById('filter');
@@ -23,6 +20,7 @@ function sortFilter() {
     }
     console.log(sortValue)
 }
+
 
 
 async function searchProduct() {
@@ -64,12 +62,11 @@ function brandRange(checkbox) {
     })
 
     let size_range=document.querySelector('input[name="brand-range"]:checked').value;
-    // console.log(size_range);
-    // query=""
     value=size_range||"Roadster"
     console.log(value)
     // sizeFetchProducts(value)
 }
+
 function priceRange(checkbox) {
     
     var checkboxes = document.getElementsByName('price-range')
@@ -79,6 +76,18 @@ function priceRange(checkbox) {
 
     let size_range=document.querySelector('input[name="price-range"]:checked').value;
     value=size_range||14803
+    console.log(value)
+    // sizeFetchProducts(value)
+}
+function colorRange(checkbox) {
+    
+    var checkboxes = document.getElementsByName('color-range')
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+
+    let size_range=document.querySelector('input[name="color-range"]:checked').value;
+    value=size_range||"black"
     console.log(value)
     // sizeFetchProducts(value)
 }
@@ -97,12 +106,12 @@ function priceRange(checkbox) {
 let querry;
 let value;
 
-// function price(){
-//     let priceVal=document.querySelector('input[name="price"]:checked').value;
-//    let priceData=+(priceVal)||500;
-//     console.log(typeof(priceData),priceData);
-//     priceFetch(priceData)
-// }
+function discountRange(){
+    let priceVal=document.querySelector('input[name="discount-range"]:checked').value;
+   let priceData=+(priceVal)||500;
+    console.log(priceData);
+    // priceFetch(priceData)
+}
 
 
 
@@ -170,7 +179,7 @@ function dataFuntion(data){
     dataContainer.innerHTML="";
    let allData= data.map((item)=>{
         
-        return`<figure class="shop-items-child-div"  >
+        return`<figure class="shop-items-child-div">
         <div class="hover-animation" data-id=${item._id} >
             <img class="img-back" src="${item["img-1"]}" alt="">
             <img class="img-front" data-id=${item._id} src="${item["img-2"]}" alt="">
@@ -180,7 +189,7 @@ function dataFuntion(data){
                     <p class="product-view-total-rating">15k</p>
                 </div>
             <div class="btn-add" >
-                <button id="wishlist-btn" onclick="cartAdd()"> <i class="fa-regular fa-heart"></i> WISHLIST</button>
+                <button id="wishlist-btn" class="wishlist-btn" data-id=${item._id}> <i class="fa-regular fa-heart"></i> <span class="change-wishlist"> WISHLIST</button>
 
             </div>
             </div>
@@ -195,20 +204,56 @@ function dataFuntion(data){
     </figure>`
     })
     dataContainer.innerHTML=allData.join(" ")
-    let click = document.querySelectorAll(".shop-items-child-div");
-      for(let btn of click){
-        // console.log(btn)
+    let img_click = document.querySelectorAll("img");
+      for(let btn of img_click){
           btn.addEventListener("click",(event)=>{ 
 			let data_id = event.target.dataset.id;
-			// console.log(data_id,typeof data_id);
-            // console.log(data_id)
+
             localStorage.setItem("product-id",data_id)
-            window.location.href="product.html"
+            window.open('product-view.html', "_blank")
 			// DeleteBtn(data_id);
+		});
+      }
+    let details_click = document.querySelectorAll("figcaption");
+      for(let btn of details_click){
+          btn.addEventListener("click",(event)=>{ 
+			let data_id = event.target.dataset.id;
+
+            localStorage.setItem("product-id",data_id)
+            window.open('product-view.html', "_blank")
+
+			// DeleteBtn(data_id);
+		});
+      }
+    let wishlist_btn = document.querySelectorAll(".wishlist-btn");
+
+    let added = false;
+      for(let btn of wishlist_btn){
+
+          btn.addEventListener("click",(event)=>{ 
+			let data_id = event.target.dataset.id;
+
+            if(added){
+                // event.target.innerText=""
+                event.target.querySelector("span").innerHTML="WISHLISTED";
+                event.target.querySelector("span").style.color="#fff";
+                event.target.querySelector("i").style.color="#ff446d"
+                event.target.style.backgroundColor="#535766"
+                    added = false;
+                }
+                else{
+                   
+                    console.log(event)
+                    console.log(data_id)
+                    event.target.querySelector("span").innerHTML="WISHLIST"
+                    event.target.style.backgroundColor="#fff";
+                    event.target.querySelector("span").style.color="black";
+                    added = true;
+                  }
 		});
       }
 }
 
-function cartAdd(){
-    console.log("hello")
-}
+// function cartAdd(){
+//     console.log("hello")
+// }
