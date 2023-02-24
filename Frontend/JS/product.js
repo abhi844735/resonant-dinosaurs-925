96ca51b9561bbfaa434834abdfada28290aa599a
Soumya Adhya <soumyaadhya007@gmail.com>
2023-02-24 23:06:54 +0530

    day 4 soumya

--- Frontend/JS/product.js
+++ Frontend/JS/product.js
@@ -1,5 +1,8 @@
-let baseurl="https://powerful-erin-jewelry.cyclic.app"
+let baseurl="http://localhost:4500";
+let producturl=`${baseurl}/products/`
 
+const cartadd=`${baseurl}/cart/add`
+const token=localStorage.getItem("token");
 
 let preloader= document.getElementById("loading");
 function loadingFun(){
@@ -8,41 +11,32 @@ function loadingFun(){
 
 
 // let size=""
-function sortFilter() {
-    let select = document.getElementById('filter');
-    let option = select.options[select.selectedIndex];
-   let sortValue = option.value;
-    if(sortValue==="acd"){
-        priceFetch(sortValue)
-    }else if(sortValue==="dcd"){
-        priceFetch(sortValue)
-    }else if(sortValue==="top"){
-        ratingFetch(sortValue)
-    }
-    console.log(sortValue)
-}
+// function sortFilter() {
+//     let select = document.getElementById('filter');
+//     let option = select.options[select.selectedIndex];
+//    let sortValue = option.value;
+//     if(sortValue==="acd"){
+//         priceFetch(sortValue)
+//     }else if(sortValue==="dcd"){
+//         priceFetch(sortValue)
+//     }else if(sortValue==="top"){
+//         ratingFetch(sortValue)
+//     }
+//     console.log(sortValue)
+// }
 
 
 
-async function searchProduct() {
-    try {
-        let searchItem = document.getElementById("search").value;
-        let allData_res = await fetch(`${baseurl}/products/search?title=${searchItem}`)
-        if (allData_res.ok) {
-            let data = await allData_res.json();;
-            // cartItme=[...data]
-            dataFuntion(data);
-            // total()
-        }
-    } catch (error) {
-        console.log(error)
-    }
+ function searchProduct() {
+        let value = document.getElementById("input").value;
+        let key="title"
+        fetchProducts(key,value)
 }
 
 
 
 function sizeRange(checkbox) {
-    
+    let key;
     var checkboxes = document.getElementsByName('size-range')
     checkboxes.forEach((item) => {
         if (item !== checkbox) item.checked = false
@@ -51,9 +45,10 @@ function sizeRange(checkbox) {
     let size_range=document.querySelector('input[name="size-range"]:checked').value;
     // console.log(size_range);
     // query=""
-    value=size_range||"tall"
+    value=size_range
+    key="category"
     console.log(value)
-    sizeFetchProducts(value)
+    fetchProducts(key,value)
 }
 function brandRange(checkbox) {
     
@@ -63,8 +58,10 @@ function brandRange(checkbox) {
     })
 
     let size_range=document.querySelector('input[name="brand-range"]:checked').value;
-    value=size_range||"Roadster"
+    value=size_range
+    key="brand"
     console.log(value)
+    fetchProducts(key,value)
     // sizeFetchProducts(value)
 }
 
@@ -76,8 +73,10 @@ function priceRange(checkbox) {
     })
 
     let size_range=document.querySelector('input[name="price-range"]:checked').value;
-    value=size_range||14803
+    value=size_range
+    key="price"
     console.log(value)
+    fetchProducts(key,value)
     // sizeFetchProducts(value)
 }
 function colorRange(checkbox) {
@@ -88,8 +87,10 @@ function colorRange(checkbox) {
     })
 
     let size_range=document.querySelector('input[name="color-range"]:checked').value;
-    value=size_range||"black"
+    value=size_range
+    key="color"
     console.log(value)
+    fetchProducts(key,value)
     // sizeFetchProducts(value)
 }
 // function sizeRange(){
@@ -109,7 +110,11 @@ let value;
 
 function discountRange(){
     let priceVal=document.querySelector('input[name="discount-range"]:checked').value;
-   let priceData=+(priceVal)||500;
+   let priceData=+(priceVal);
+   value=+(priceVal)
+    key="discount"
+    console.log(value)
+    fetchProducts(key,value)
     console.log(priceData);
     // priceFetch(priceData)
 }
@@ -117,51 +122,50 @@ function discountRange(){
 
 
 
-async function ratingFetch(sortValue){
-try {
-        let res= await fetch(`${baseurl}/products/rating?rating=${sortValue}`);
-        if(res.ok){
-            let data=await res.json();
-            dataFuntion(data)
-            // console.log(data)
-        }
+// async function ratingFetch(sortValue){
+// try {
+//         let res= await fetch(`${baseurl}/products/rating?rating=${sortValue}`);
+//         if(res.ok){
+//             let data=await res.json();
+//             dataFuntion(data)
+//             // console.log(data)
+//         }
     
-} catch (error) {
-    console.log(error)
-    // alert("Fetching problem")
-}
-}
-async function priceFetch(priceData){
-try {
-        let res= await fetch(`${baseurl}/products/price?price=${priceData}`);
-        if(res.ok){
-            let data=await res.json();
-            dataFuntion(data)
-            // console.log(data)
-        }
+// } catch (error) {
+//     console.log(error)
+//     // alert("Fetching problem")
+// }
+// }
+// async function priceFetch(priceData){
+// try {
+//         let res= await fetch(`${baseurl}/products/price?price=${priceData}`);
+//         if(res.ok){
+//             let data=await res.json();
+//             dataFuntion(data)
+//             // console.log(data)
+//         }
     
-} catch (error) {
-    console.log(error)
-    // alert("Fetching problem")
-}
-}
-async function sizeFetchProducts(value){
-try {
-        let res= await fetch(`${baseurl}/products?sizerange=${value}`);
-        if(res.ok){
-            let data=await res.json();
-            dataFuntion(data)
-            // console.log(data)
-        }
+// } catch (error) {
+//     console.log(error)
+//     // alert("Fetching problem")
+// }
+// }
+// async function sizeFetchProducts(value){
+// try {
+//         let res= await fetch(`${baseurl}/products?sizerange=${value}`);
+//         if(res.ok){
+//             let data=await res.json();
+//             dataFuntion(data)
+//         }
     
-} catch (error) {
-    console.log(error)
-    // alert("Fetching problem")
-}
-}
-async function fetchProducts(){
+// } catch (error) {
+//     console.log(error)
+// }
+// }
+async function fetchProducts(key,value){
 try {
-        let res= await fetch(`${baseurl}/products`);
+        // let res= await fetch(`${producturl}/search?${key||"gender"}=${value||"male"}`);
+        let res= await fetch(`${producturl}/search?gender=male&${key}=${value}`)
         if(res.ok){
             let data=await res.json();
             dataFuntion(data)
@@ -170,7 +174,6 @@ try {
     
 } catch (error) {
     console.log(error)
-    // alert("Fetching problem")
 }
 }
 fetchProducts()
@@ -182,24 +185,23 @@ function dataFuntion(data){
         
         return`<figure class="shop-items-child-div">
         <div class="hover-animation" data-id=${item._id} >
-            <img class="img-back" src="${item["img-1"]}" alt="">
-            <img class="img-front" data-id=${item._id} src="${item["img-2"]}" alt="">
+            <img class="img-back" src="${item.image_1}" alt="">
+            <img class="img-front" data-id=${item._id} src="${item.image_2}" alt="">
             <div class="product-view-rating-div">
-                        <p class="product-view-star" >4 <span><i class="fa-solid fa-star"></i></span></p>
+                        <p class="product-view-star" >${Math.floor(Math.random() * 5) + 1}<span><i class="fa-solid fa-star"></i></span></p>
                         <hr>
-                    <p class="product-view-total-rating">15k</p>
+                    <p class="product-view-total-rating">${Math.floor(Math.random() * 5) + 1}k</p>
                 </div>
             <div class="btn-add" >
                 <button id="wishlist-btn" class="wishlist-btn" data-id=${item._id}> <i class="fa-regular fa-heart"></i> <span class="change-wishlist"> WISHLIST</button>
-
             </div>
             </div>
             <figcaption>
-            <p>${item.title}</p>
+            <p>${item.description}</p>
             <div class="similar-products-price-div">
-            <h5>Rs.365</h5>
-            <strike>Rs. 599</strike>
-            <p>(39% OFF)</p>
+            <h5>Rs.${item.price}</h5>
+            <strike>Rs. ${item.price+500}</strike>
+            <p>(${item.discount}% OFF)</p>
         </div>
         </figcaption>
     </figure>`
@@ -231,16 +233,60 @@ function dataFuntion(data){
     let added = false;
       for(let btn of wishlist_btn){
 
-          btn.addEventListener("click",(event)=>{ 
+          btn.addEventListener("click",async (event)=>{ 
 			let data_id = event.target.dataset.id;
 
             if(added){
                 // event.target.innerText=""
-                event.target.querySelector("span").innerHTML="WISHLISTED";
-                event.target.querySelector("span").style.color="#fff";
-                event.target.querySelector("i").style.color="#ff446d"
-                event.target.style.backgroundColor="#535766"
-                    added = false;
+                // async function addToCart(){
+                    try {
+                
+                        let res = await fetch(`${cartadd}/${data_id}`, {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                                Authorization:token ,
+                            },
+                            // body: JSON.stringify(obj)
+                        })
+                        let msg=await res.json()
+                        // console.log(await res.json())
+                
+                        if (msg.message=="Product added to cart") {
+                            event.target.querySelector("span").innerHTML="WISHLISTED";
+                            event.target.querySelector("span").style.color="#fff";
+                            event.target.querySelector("i").style.color="#ff446d"
+                            event.target.style.backgroundColor="#535766"
+                            added = false;
+                            alert("Added To cart");
+                            // window.location.reload()
+                            // return
+                            // container.innerHTML=""
+                        }
+                        if(msg.message=="Access Denied"){
+                            return alert("Please log in or create an account to add this item to your bag.")
+                        }
+                        if(msg.message=="Product already in cart"){
+                            // container.innerHTML=""
+                            // let add_to_bag=document.getElementById("add-to-cart-btn")
+                            // let go_to_bag=document.getElementById("go-to-cart-btn");
+                            // add_to_bag.style.display="none"
+                            // go_to_bag.style.display="block"
+                            return alert("Product already in your bag")
+                        }
+                
+                    }
+                    catch (error) {
+                        console.log(error)
+                    }
+                // }
+
+
+                // event.target.querySelector("span").innerHTML="WISHLISTED";
+                // event.target.querySelector("span").style.color="#fff";
+                // event.target.querySelector("i").style.color="#ff446d"
+                // event.target.style.backgroundColor="#535766"
+                //     added = false;
                 }
                 else{
                    
@@ -261,7 +307,7 @@ function dataFuntion(data){
 document.querySelector("#cl233").addEventListener("click",()=>{
     window.location.href="cart_page.html"
 })
-let token=localStorage.getItem("token");
+// let token=localStorage.getItem("token");
 document.getElementById("line1").innerText=localStorage.getItem("name")||"Welcome";
 if(token){
     document.querySelector(".login").style.display="none"
