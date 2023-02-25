d45a65758b4ab6a1db6dec8d0df24861581e3f30
Darshan <darshannas11@gmail.com>
2023-02-25 21:04:10 +0530

    day-5

--- Backend/routes/product.router.js
+++ Backend/routes/product.router.js
@@ -12,7 +12,7 @@ productRouter.get("/", async (req, res) => {
         const data = await ProductModel.find();
         res.send(data);
     } catch (error) {
-        res.status(500).send({message: error.message})
+        res.status(500).send({ message: error.message })
     }
 })
 
@@ -23,25 +23,26 @@ productRouter.post("/add", authentication, authorization, async (req, res) => {
         await product.save()
         res.send({ message: "Product added" });
     } catch (error) {
-        return res.status(500).send({message: error.message})
+        return res.status(500).send({ message: error.message })
     }
 });
 
 productRouter.get('/search', async (req, res) => {
     const payload = req.query;
-    if(payload.title) {
+    if (payload.title) {
+        let title = payload.title.toLowerCase();
         try {
-            const products = await ProductModel.find({title: {$regex: payload.title}});
+            const products = await ProductModel.find({ title: { $regex: '(?i)' + title } });
             return res.send(products)
         } catch (error) {
-            return res.send({message: error.message})
+            return res.send({ message: error.message })
         }
     }
     try {
         const products = await ProductModel.find(payload)
         res.send(products)
     } catch (error) {
-        res.status(500).send({message: error.message})
+        res.status(500).send({ message: error.message })
     }
 })
 
@@ -49,15 +50,15 @@ productRouter.get('/search', async (req, res) => {
 productRouter.get("/:id", idvalidator, async (req, res) => {
     const id = req.params['id'];
     try {
-        const product = await ProductModel.findOne({_id: id})
+        const product = await ProductModel.findOne({ _id: id })
         res.send(product)
     } catch (error) {
-        res.status(500).send({message: error.message})
+        res.status(500).send({ message: error.message })
     }
 })
 
 
-productRouter.patch("/update/:id", authentication, authorization, idvalidator, async (req, res) => {
+productRouter.put("/update/:id", authentication, authorization, idvalidator, async (req, res) => {
     let id = req.params.id;
     const update = req.body;
     try {
