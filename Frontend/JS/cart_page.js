93864e0365ca0cf41372189a927fa58dbe18ce41
Rudra <merudramanu@gmail.com>
2023-04-08 23:55:18 +0530

    added cart latest

--- Frontend/JS/cart_page.js
+++ Frontend/JS/cart_page.js
@@ -14,224 +14,225 @@ let subTotal = document.getElementById("ttl");
 
 let totalSaving = document.getElementById("ttl_dsc");
 //=================code for append========================================
-let append = (data) => {
-  discount = data.length * 99;
-  data.forEach((el) => {
-    let id = el._id;
-    let n = 1;
-    //creating a div for each and every products added in the cart
-    let div = document.createElement("div");
-    div.setAttribute("id", "items");
+let append = (el) => {
+  discount += 99;
+  // items_in_cart.innerHTML = null;
+  // data.forEach((el) => {
+  let id = el._id;
+  let n = 1;
+  //creating a div for each and every products added in the cart
+  let div = document.createElement("div");
+  div.setAttribute("id", "items");
 
-    //================image in divleft=====================================
-    let divleft = document.createElement("div");
-    divleft.setAttribute("id", "photo_div");
+  //================image in divleft=====================================
+  let divleft = document.createElement("div");
+  divleft.setAttribute("id", "photo_div");
 
-    let img = document.createElement("img");
-    img.setAttribute("id", "itemincart");
-    img.src = el.url;
+  let img = document.createElement("img");
+  img.setAttribute("id", "itemincart");
+  img.src = el.url;
 
-    divleft.append(img);
-    //===================details in divright=================================
-    let divright = document.createElement("div");
-    divright.setAttribute("id", "details_div");
-    //=====================brand in div1=====================================
-    let div1 = document.createElement("div");
-    div1.setAttribute("id", "name_div");
+  divleft.append(img);
+  //===================details in divright=================================
+  let divright = document.createElement("div");
+  divright.setAttribute("id", "details_div");
+  //=====================brand in div1=====================================
+  let div1 = document.createElement("div");
+  div1.setAttribute("id", "name_div");
 
-    let name = document.createElement("p");
-    name.innerText = el.name;
+  let name = document.createElement("p");
+  name.innerText = el.name;
 
-    div1.append(name);
-    //=================description in div2====================================
-    let div2 = document.createElement("div");
-    div2.setAttribute("id", "desc_div");
+  div1.append(name);
+  //=================description in div2====================================
+  let div2 = document.createElement("div");
+  div2.setAttribute("id", "desc_div");
 
-    let desc = document.createElement("p");
-    desc.innerText = el.description.substring(0, 20) + "...";
+  let desc = document.createElement("p");
+  desc.innerText = el.description.substring(0, 20) + "...";
 
-    div2.append(desc);
-    //=================mrp in div3============================================
+  div2.append(desc);
+  //=================mrp in div3============================================
 
-    let div3 = document.createElement("div");
-    div3.setAttribute("id", "mrp_div");
+  let div3 = document.createElement("div");
+  div3.setAttribute("id", "mrp_div");
 
-    let MRP = document.createElement("p");
-    MRP.innerText = "MRP";
+  let MRP = document.createElement("p");
+  MRP.innerText = "MRP";
 
-    let MRPprice = document.createElement("p");
-    MRPprice.innerText = `Rs. ${+el.price + 99}.00/-`;
-    totalMRP = totalMRP + (+el.price + 99);
-    localStorage.setItem("totalcartvalue", JSON.stringify(totalMRP - discount));
-    localStorage.setItem("totalmrp", JSON.stringify(totalMRP));
-    localStorage.setItem("discount", JSON.stringify(discount));
-    MRPprice.style.textDecoration = "line-through";
+  let MRPprice = document.createElement("p");
+  MRPprice.innerText = `Rs. ${+el.price + 99}.00/-`;
+  totalMRP = totalMRP + (+el.price + 99);
+  localStorage.setItem("totalcartvalue", JSON.stringify(totalMRP - discount));
+  localStorage.setItem("totalmrp", JSON.stringify(totalMRP));
+  localStorage.setItem("discount", JSON.stringify(discount));
+  MRPprice.style.textDecoration = "line-through";
 
-    div3.append(MRP, MRPprice);
-    //================sell price in div4================================
+  div3.append(MRP, MRPprice);
+  //================sell price in div4================================
 
-    let div4 = document.createElement("div");
-    div4.setAttribute("id", "sell_div");
-    let Sell_price = document.createElement("p");
-    Sell_price.innerText = "Sell Price";
+  let div4 = document.createElement("div");
+  div4.setAttribute("id", "sell_div");
+  let Sell_price = document.createElement("p");
+  Sell_price.innerText = "Sell Price";
 
-    let price = document.createElement("p");
-    price.innerText = `Rs. ${+el.price}.00/-`;
-    total = total + +el.price;
+  let price = document.createElement("p");
+  price.innerText = `Rs. ${+el.price}.00/-`;
+  total = total + +el.price;
 
-    div4.append(Sell_price, price);
+  div4.append(Sell_price, price);
 
-    //=================remove add in div5================================
-    let div5 = document.createElement("div");
-    div5.setAttribute("id", "addremove");
+  //=================remove add in div5================================
+  let div5 = document.createElement("div");
+  div5.setAttribute("id", "addremove");
 
-    let div5l = document.createElement("div");
-    div5l.setAttribute("id", "remove");
+  let div5l = document.createElement("div");
+  div5l.setAttribute("id", "remove");
 
-    let remove = document.createElement("img");
-    remove.src = "https://img.1mg.com/images/delete_icon.svg";
-    remove.style.cursor = "pointer";
-    div5l.append(remove);
+  let remove = document.createElement("img");
+  remove.src = "https://img.1mg.com/images/delete_icon.svg";
+  remove.style.cursor = "pointer";
+  div5l.append(remove);
 
-    remove.addEventListener("click", () => {
+  remove.addEventListener("click", () => {
+    alert(`${el.name} will be removed from list`);
+    del_cart_Data(id);
+  });
+
+  let div5r = document.createElement("div");
+  div5r.setAttribute("id", "add");
+
+  let div5r1 = document.createElement("div");
+  let minus = document.createElement("img");
+  minus.src =
+    "https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/512/external-glyph-shapes-tanah-basah-glyph-tanah-basah-32.png";
+  minus.style.width = "15px";
+
+  minus.style.cursor = "pointer";
+  div5r1.append(minus);
+
+  minus.addEventListener("click", () => {
+    n--;
+
+    if (flag == false) {
+      price.innerText = `Rs. ${+el.price * n}.00/-`;
+      MRPprice.innerText = `Rs. ${(+el.price + 99) * n}.00/-`;
+
+      totalMRP = totalMRP - (+el.price + 99);
+      localStorage.setItem(
+        "totalcartvalue",
+        JSON.stringify(totalMRP - discount)
+      );
+      //setting the total mrp in local storage=====================================
+      localStorage.setItem("totalmrp", JSON.stringify(totalMRP));
+
+      totalmrp.innerText = `Rs. ${totalMRP}/-`;
+      discount = discount - 99;
+      localStorage.setItem("discount", JSON.stringify(discount));
+      priceDiscount.innerText = `- Rs. ${discount}/-`;
+      subTotal.innerText = `Rs. ${totalMRP - discount}.00/-`;
+      totalSaving.innerText = `Rs. ${discount}/-`;
+    } else {
+      price.innerText = `Rs. ${+el.price * n}.00/-`;
+      MRPprice.innerText = `Rs. ${(+el.price + 99) * n}.00/-`;
+
+      totalMRP = totalMRP - (+el.price + 99);
+      localStorage.setItem(
+        "totalcartvalue",
+        JSON.stringify(totalMRP - discount)
+      );
+      localStorage.setItem("totalmrp", JSON.stringify(totalMRP));
+
+      totalmrp.innerText = `Rs. ${totalMRP}/-`;
+      discount = discount - 99;
+      localStorage.setItem("discount", JSON.stringify(discount));
+      priceDiscount.innerText = `- Rs. ${discount}/-`;
+      subTotal.innerText = `Rs. ${totalMRP - discount - 200}.00/-`;
+      totalSaving.innerText = `Rs. ${discount + 200}/-`;
+    }
+    if (n >= 1) {
+      p2.innerText = n;
+    } else if (n == 0) {
+      //if n==0 then that product will be deleted from cart list
       alert(`${el.name} will be removed from list`);
       del_cart_Data(id);
-    });
-
-    let div5r = document.createElement("div");
-    div5r.setAttribute("id", "add");
-
-    let div5r1 = document.createElement("div");
-    let minus = document.createElement("img");
-    minus.src =
-      "https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/512/external-glyph-shapes-tanah-basah-glyph-tanah-basah-32.png";
-    minus.style.width = "15px";
-
-    minus.style.cursor = "pointer";
-    div5r1.append(minus);
-
-    minus.addEventListener("click", () => {
-      n--;
-
-      if (flag == false) {
-        price.innerText = `Rs. ${+el.price * n}.00/-`;
-        MRPprice.innerText = `Rs. ${(+el.price + 99) * n}.00/-`;
-
-        totalMRP = totalMRP - (+el.price + 99);
-        localStorage.setItem(
-          "totalcartvalue",
-          JSON.stringify(totalMRP - discount)
-        );
-        //setting the total mrp in local storage=====================================
-        localStorage.setItem("totalmrp", JSON.stringify(totalMRP));
-
-        totalmrp.innerText = `Rs. ${totalMRP}/-`;
-        discount = discount - 99;
-        localStorage.setItem("discount", JSON.stringify(discount));
-        priceDiscount.innerText = `- Rs. ${discount}/-`;
-        subTotal.innerText = `Rs. ${totalMRP - discount}.00/-`;
-        totalSaving.innerText = `Rs. ${discount}/-`;
-      } else {
-        price.innerText = `Rs. ${+el.price * n}.00/-`;
-        MRPprice.innerText = `Rs. ${(+el.price + 99) * n}.00/-`;
-
-        totalMRP = totalMRP - (+el.price + 99);
-        localStorage.setItem(
-          "totalcartvalue",
-          JSON.stringify(totalMRP - discount)
-        );
-        localStorage.setItem("totalmrp", JSON.stringify(totalMRP));
-
-        totalmrp.innerText = `Rs. ${totalMRP}/-`;
-        discount = discount - 99;
-        localStorage.setItem("discount", JSON.stringify(discount));
-        priceDiscount.innerText = `- Rs. ${discount}/-`;
-        subTotal.innerText = `Rs. ${totalMRP - discount - 200}.00/-`;
-        totalSaving.innerText = `Rs. ${discount + 200}/-`;
-      }
-      if (n >= 1) {
-        p2.innerText = n;
-      } else if (n == 0) {
-        //if n==0 then that product will be deleted from cart list
-        alert(`${el.name} will be removed from list`);
-        del_cart_Data(id);
-      }
-    });
-
-    let div5r2 = document.createElement("div");
-    let p2 = document.createElement("h3");
-    p2.innerText = n;
-    div5r2.append(p2);
-
-    let div5r3 = document.createElement("div");
-    let plus = document.createElement("img");
-    plus.src = "https://img.icons8.com/tiny-glyph/512/plus-math.png";
-    plus.style.width = "15px";
-    plus.style.cursor = "pointer";
-    div5r3.append(plus);
-
-    plus.addEventListener("click", () => {
-      n++;
-      if (n <= 10) {
-        p2.innerText = n;
-      }
-      if (n > 10) {
-        n = 10;
-        return alert(
-          "Sorry, You Can't Add More Than 10 Products of Same Type!!!"
-        );
-      }
-      if (flag == false) {
-        price.innerText = `Rs. ${+el.price * n}.00/-`;
-
-        MRPprice.innerText = `Rs. ${(+el.price + 99) * n}.00/-`;
-        total = total + +el.price * n;
-        totalMRP = totalMRP + (+el.price + 99);
-        localStorage.setItem(
-          "totalcartvalue",
-          JSON.stringify(totalMRP - discount)
-        );
-        localStorage.setItem("totalmrp", JSON.stringify(totalMRP));
-
-        discount = discount + 99;
-        localStorage.setItem("discount", JSON.stringify(discount));
-        totalmrp.innerText = `Rs. ${totalMRP}/-`;
-
-        priceDiscount.innerText = `- Rs. ${discount}/-`;
-
-        subTotal.innerText = `Rs. ${totalMRP - discount}.00/-`;
-
-        totalSaving.innerText = `Rs. ${discount}/-`;
-      } else {
-        price.innerText = `Rs. ${+el.price * n}.00/-`;
-
-        MRPprice.innerText = `Rs. ${(+el.price + 99) * n}.00/-`;
-        total = total + +el.price * n;
-        totalMRP = totalMRP + (+el.price + 99);
-        localStorage.setItem(
-          "totalcartvalue",
-          JSON.stringify(totalMRP - discount)
-        );
-        localStorage.setItem("totalmrp", JSON.stringify(totalMRP));
-
-        discount = discount + 99;
-        localStorage.setItem("discount", JSON.stringify(discount));
-        totalmrp.innerText = `Rs. ${totalMRP}/-`;
-
-        priceDiscount.innerText = `- Rs. ${discount}/-`;
-
-        subTotal.innerText = `Rs. ${totalMRP - discount - 200}.00/-`;
-
-        totalSaving.innerText = `Rs. ${discount + 200}/-`;
-        //===================================
-      }
-    });
-    div5r.append(div5r1, div5r2, div5r3);
-    div5.append(div5l, div5r);
-    divright.append(div1, div2, div3, div4, div5);
-    div.append(divleft, divright);
-    items_in_cart.append(div);
+    }
   });
+
+  let div5r2 = document.createElement("div");
+  let p2 = document.createElement("h3");
+  p2.innerText = n;
+  div5r2.append(p2);
+
+  let div5r3 = document.createElement("div");
+  let plus = document.createElement("img");
+  plus.src = "https://img.icons8.com/tiny-glyph/512/plus-math.png";
+  plus.style.width = "15px";
+  plus.style.cursor = "pointer";
+  div5r3.append(plus);
+
+  plus.addEventListener("click", () => {
+    n++;
+    if (n <= 10) {
+      p2.innerText = n;
+    }
+    if (n > 10) {
+      n = 10;
+      return alert(
+        "Sorry, You Can't Add More Than 10 Products of Same Type!!!"
+      );
+    }
+    if (flag == false) {
+      price.innerText = `Rs. ${+el.price * n}.00/-`;
+
+      MRPprice.innerText = `Rs. ${(+el.price + 99) * n}.00/-`;
+      total = total + +el.price * n;
+      totalMRP = totalMRP + (+el.price + 99);
+      localStorage.setItem(
+        "totalcartvalue",
+        JSON.stringify(totalMRP - discount)
+      );
+      localStorage.setItem("totalmrp", JSON.stringify(totalMRP));
+
+      discount = discount + 99;
+      localStorage.setItem("discount", JSON.stringify(discount));
+      totalmrp.innerText = `Rs. ${totalMRP}/-`;
+
+      priceDiscount.innerText = `- Rs. ${discount}/-`;
+
+      subTotal.innerText = `Rs. ${totalMRP - discount}.00/-`;
+
+      totalSaving.innerText = `Rs. ${discount}/-`;
+    } else {
+      price.innerText = `Rs. ${+el.price * n}.00/-`;
+
+      MRPprice.innerText = `Rs. ${(+el.price + 99) * n}.00/-`;
+      total = total + +el.price * n;
+      totalMRP = totalMRP + (+el.price + 99);
+      localStorage.setItem(
+        "totalcartvalue",
+        JSON.stringify(totalMRP - discount)
+      );
+      localStorage.setItem("totalmrp", JSON.stringify(totalMRP));
+
+      discount = discount + 99;
+      localStorage.setItem("discount", JSON.stringify(discount));
+      totalmrp.innerText = `Rs. ${totalMRP}/-`;
+
+      priceDiscount.innerText = `- Rs. ${discount}/-`;
+
+      subTotal.innerText = `Rs. ${totalMRP - discount - 200}.00/-`;
+
+      totalSaving.innerText = `Rs. ${discount + 200}/-`;
+      //===================================
+    }
+  });
+  div5r.append(div5r1, div5r2, div5r3);
+  div5.append(div5l, div5r);
+  divright.append(div1, div2, div3, div4, div5);
+  div.append(divleft, divright);
+  items_in_cart.append(div);
+  // });
   //======these four line is written here just to initiate the price,discount etc========================
   totalmrp.innerText = `Rs. ${totalMRP}/-`;
   priceDiscount.innerText = `- Rs. ${discount}/-`;
@@ -277,8 +278,8 @@ let append = (data) => {
 
 function get_cart_data() {
   const url = "https://excited-deer-headscarf.cyclic.app";
-  const fetchCart = async (url) => {
-    const res = await fetch(url, {
+  const fetchCart = async () => {
+    const res = await fetch(`${url}/cart`, {
       method: "GET",
       headers: {
         Authorization: localStorage.getItem("token"),
@@ -286,36 +287,37 @@ function get_cart_data() {
     });
     if (res.ok) {
       const data = await res.json();
+      // console.log("gygy", data);
       if (data.length == 0) {
         alert("Cart is empty\u{1F6D2}\nRedirecting you to Homepage");
         window.location.href = "./index.html";
       } else {
-        const arr = [];
         //after fetching data we are storing all the data in array "arr" and arr will be appended in frontend page
-        data.forEach(async (index) => {
+        // let arr = [];
+        data.map(async (item) => {
+          // console.log(item.productId);
+          let productRes = await fetch(`${url}/products/${item.productId}`);
+          let product = await productRes.json();
+
+          // console.log("xxxxxxxxxx", product);
+
           let obj = {};
-          let product = await getProduct(index.productId);
-          // console.log(product);
+
           obj._id = product._id;
           obj.name = product.title;
           obj.description = product.description;
           obj.price = product.price;
           obj.url = product.image_1;
-          obj.quantity = index.quantity;
-          arr.push(obj);
+          console.log("obj", obj);
+          // arr.push(obj);
           // we are invoking the append function here
-          append(arr);
+          // console.log("arrrrrr", arr);
+          append(obj);
         });
-        console.log("data:", arr);
       }
     }
   };
-  const getProduct = async (id) => {
-    const product = await fetch(url + "/products/" + id);
-    const res = await product.json();
-    return res;
-  };
-  fetchCart(url + "/cart");
+  fetchCart();
 }
 get_cart_data();
 //===============delete function================
--- Frontend/JS/signup.js
+++ Frontend/JS/signup.js
@@ -4,79 +4,82 @@
 import navbar from "../components/header.js";
 let div3 = document.querySelector(".header");
 div3.innerHTML = navbar();
-document.addEventListener('DOMContentLoaded', () => {
-    
-    const Big_screen_sreachbar=document.getElementById("search_bar");
-    const small_screen_sreachbar=document.querySelector(".input-box");
-    Big_screen_sreachbar.style.display="none"
-    small_screen_sreachbar.style.display="none"
-})
-
-
-let signup_btn=document.getElementById("signup_form");
-signup_btn.addEventListener("submit",async(e)=>{
-    e.preventDefault();
-    let first_name=document.getElementById("first_name").value;
-    let last_name=document.getElementById("last_name").value;
-let mobile = document.getElementById("mobile").value;
-mobile= parseInt(mobile)
-let email = document.getElementById("email").value;
-let gender = document.getElementById("gender").value;
-let password=document.getElementById("password").value;
- if(!(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*['@', '$', '#']).*$/.test(password))){
-   await swal("should contains a lowercase, uppercase, number and any of the special characters")
-}
-
-else{
-    let obj={
-        first_name,last_name,mobile,email,gender,password
-    }
-    console.log(obj)
-    signup(obj);
-}
-
+document.addEventListener("DOMContentLoaded", () => {
+  const Big_screen_sreachbar = document.getElementById("search_bar");
+  const small_screen_sreachbar = document.querySelector(".input-box");
+  Big_screen_sreachbar.style.display = "none";
+  small_screen_sreachbar.style.display = "none";
 });
-let signup =async(obj)=>{
-   let res = await fetch("http://localhost:4500/users/signup",{
-    method:"POST",
-    headers:{
-        "Content-Type":"application/json"
+
+let signup_btn = document.getElementById("signup_form");
+signup_btn.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  let first_name = document.getElementById("first_name").value;
+  let last_name = document.getElementById("last_name").value;
+  let mobile = document.getElementById("mobile").value;
+  mobile = parseInt(mobile);
+  let email = document.getElementById("email").value;
+  let gender = document.getElementById("gender").value;
+  let password = document.getElementById("password").value;
+  if (
+    !/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*['@', '$', '#']).*$/.test(password)
+  ) {
+    await swal(
+      "should contains a lowercase, uppercase, number and any of the special characters"
+    );
+  } else {
+    let obj = {
+      first_name,
+      last_name,
+      mobile,
+      email,
+      gender,
+      password,
+    };
+    console.log(obj);
+    signup(obj);
+  }
+});
+let signup = async (obj) => {
+  let res = await fetch("http://localhost:4500/users/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
-    body:JSON.stringify(obj)
-   });
-   let data = await res.json();
-   if(res.ok){
+    body: JSON.stringify(obj),
+  });
+  let data = await res.json();
+  if (res.ok) {
     await swal(data.message);
-    window.location.href="login.html"
-   }else{
-   await swal(data.message);
-   }
+    window.location.href = "login.html";
+  } else {
+    await swal(data.message);
+  }
+};
+let token = localStorage.getItem("token");
+document.getElementById("line1").innerText =
+  localStorage.getItem("name") || "Welcome";
+if (token) {
+  document.querySelector(".login").style.display = "none";
+  if (token) {
+    document.querySelector(".logout").addEventListener("click", async (e) => {
+      e.preventDefault();
+      let res = await fetch("http://localhost:4500/users/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: localStorage.getItem("token"),
+        },
+      });
+      let data = await res.json();
+      if (data.message == "Logout Sucessfull") {
+        await swal("log out succussfully");
+        localStorage.clear();
+        window.location.reload();
+      }
+    });
+  }
+} else {
+  document.querySelector(".login").style.display = "block";
+  document.querySelector(".logout").style.display = "none";
 }
-let token=localStorage.getItem("token");
-document.getElementById("line1").innerText=localStorage.getItem("name")||"Welcome";
-if(token){
-    document.querySelector(".login").style.display="none"
-    if(token){
-        document.querySelector(".logout").addEventListener("click",async(e)=>{
-            e.preventDefault();
-         let res= await   fetch("http://localhost:4500/users/logout",{
-                method:"POST",
-                headers:{
-                    "Content-Type":"application/json",
-                    "authorization":localStorage.getItem("token")
-                }
-            })
-        let data = await res.json();
-        if(data.message=="Logout Sucessfull"){
-              await  swal("log out succussfully");
-                localStorage.clear();
-                window.location.reload()
-        }
-        })
-    }
-}else{
-    document.querySelector(".login").style.display="block";
-    document.querySelector(".logout").style.display="none";
-}
- 
- 
